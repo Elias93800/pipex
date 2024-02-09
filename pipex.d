31c6cf147800587d4e5c2a5713c7bcc86b5127a9
Elias Mehdaoui <emehdaou@bess-f2r1s14.clusters.42paris.fr>
2024-02-09 07:23:04 +0100

    manque derniers tests sinon tout ok

---
+++ pipex.d
@@ -0,0 +1,2 @@
+pipex: srcs/bonus/utils.c srcs/bonus/../../includes/pipex.h \
+  includes/../libft/libft.h
---
+++ srcs/mandatory/utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emehdaou <emehdaou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 03:56:58 by emehdaou          #+#    #+#             */
+/*   Updated: 2024/02/07 22:58:32 by emehdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/pipex.h"
+
+char	*ft_findpath(char **envp)
+{
+	int	i;
+
+	i = 0;
+	if (!envp)
+		return (NULL);
+	while (envp[i])
+	{
+		if (!strncmp(envp[i], "PATH=", 5))
+			return (envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_path(char **envp, char *cmd)
+{
+	int		i;
+	char	**path;
+	char	*tmp;
+
+	i = 0;
+	if (ft_strchr(cmd, '/'))
+		return (ft_strdup(cmd));
+	path = ft_split(&ft_findpath(envp)[5], ':');
+	if (!path)
+		return (cmd);
+	while (path[i])
+	{
+		tmp = ft_strjoin(path[i], cmd);
+		if (access(tmp, F_OK | X_OK) == 0)
+			return (free_tab(path), tmp);
+		free(tmp);
+		i++;
+	}
+	return (free_tab(path), NULL);
+}
+
+void	ft_init(int argc, char **argv, char **envp, t_data *data)
+{
+	data->nbcmd = argc - 3;
+	data->infile = argv[1];
+	data->outfile = argv[argc - 1];
+	data->envp = envp;
+	data->cmd = &argv[2];
+}
+
+void	ft_waitpid(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nbcmd)
+		waitpid(data->pid[i++], NULL, 0);
+}
